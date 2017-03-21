158d595299dc1b58d980c77c4c3c4077ee210ecf
Benjamin Saphier <b.saphier@gmail.com>
2017-03-21 17:20:54 -0400

    check if ternary improves performance

--- speedLoops.js
+++ speedLoops.js
@@ -1,39 +1,46 @@
 inlets  = 3;
 outlets = 2;
+
 // ––––––––––––––––––––––––––––––––– \\
 
-// percent change per unit
-var ppu     = 1.45;
-
-var MAX_KPH = 100;
-var STEP    = 127 / MAX_KPH;
+var DELTA   = 1.45;          // percent of change per unit
+var MAX_KPH = 100;           // range of the "speedometer"
+var STEP    = 127 / MAX_KPH; // the number of MIDI "ticks" per unit
 
 
-// parse the input
+// "parse" the input
 function msg_float (num) {
 
-  if (inlet === 0) {
-    modulate(num);
-  }
+  inlet === 0
+    ? modulate(num)
+    : inlet === 1
+      ? this.DELTA   = num
+      : this.MAX_KPH = num;
 
-  if (inlet === 1) {
-    this.ppu = num;
-  }
-
-  if (inlet === 3) {
-    this.MAX_KPH = num;
-  }
+  // samesame but different
+  // if (inlet === 0) {
+  //   modulate(num);
+  // }
+  //
+  // if (inlet === 1) {
+  //   this.DELTA = num;
+  // }
+  //
+  // if (inlet === 3) {
+  //   this.MAX_KPH = num;
+  // }
 }
 
-//
+// modulate the input number
 function modulate (num) {
 
-  var _normToMaxKPH = num / STEP;
+  var _normToMaxKPH = num / this.STEP;
 
+  // round the float to nearest integer
   var normToMaxKPH = (_normToMaxKPH % 1 > 0.5)
     ? _normToMaxKPH - (_normToMaxKPH % 1)
     : _normToMaxKPH + (1 - _normToMaxKPH % 1):
 
-  outlet(0, normToMaxKPH);
-  outlet(1, _normToMaxKPH);
+  outlet(0, deltaSpeedPercentage); // output to pitch-bend module
+  outlet(1, normToMaxKPH);         // output to "speedometer"
 }
