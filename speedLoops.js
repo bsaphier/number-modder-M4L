3e7110fd63a4e87b92c042d28c5e820f037bd8fe
Benjamin Saphier <b.saphier@gmail.com>
2017-03-22 10:56:41 -0400

    fixed float rounding and bangs only on int change

--- speedLoops.js
+++ speedLoops.js
@@ -3,44 +3,47 @@ outlets = 2;
 
 // ––––––––––––––––––––––––––––––––– \\
 
-var DELTA   = 1.45;          // percent of change per unit
+var DELTA   = 1.45 / 100;    // percent of change per unit
 var MAX_KPH = 100;           // range of the "speedometer"
 var STEP    = 127 / MAX_KPH; // the number of MIDI "ticks" per unit
 
+var prevNormToMaxKPH;
 
 // "parse" the input
 function msg_float (num) {
 
-  inlet === 0
-    ? modulate(num)
-    : inlet === 1
-      ? this.DELTA   = num
-      : this.MAX_KPH = num;
+  if (inlet === 0) {
+    modulate(num);
+  }
 
-  // samesame but different
-  // if (inlet === 0) {
-  //   modulate(num);
-  // }
-  //
-  // if (inlet === 1) {
-  //   this.DELTA = num;
-  // }
-  //
-  // if (inlet === 3) {
-  //   this.MAX_KPH = num;
-  // }
+  if (inlet === 1) {
+    this.DELTA = num / 100;
+  }
+
+  if (inlet === 3) {
+    this.MAX_KPH = num;
+  }
 }
 
 // modulate the input number
 function modulate (num) {
 
-  var _normToMaxKPH = num / this.STEP;
+  var _normalToMaxKPH = num / this.STEP;
+
+  post(num, '\n');
 
   // round the float to nearest integer
-  var normToMaxKPH = (_normToMaxKPH % 1 > 0.5)
-    ? _normToMaxKPH - (_normToMaxKPH % 1)
-    : _normToMaxKPH + (1 - _normToMaxKPH % 1):
+  var normalToMaxKPH = ((_normalToMaxKPH % 1 > 0.5)
+    ? _normalToMaxKPH - (_normalToMaxKPH % 1)
+    : _normalToMaxKPH + (1 - _normalToMaxKPH % 1))
+    - 1;
 
-  outlet(0, deltaSpeedPercentage); // output to pitch-bend module
-  outlet(1, normToMaxKPH);         // output to "speedometer"
+  var deltaSpeedPercentage = normalToMaxKPH * DELTA;
+
+  outlet(0, deltaSpeedPercentage);    // output to pitch-bend module
+
+  if (this.prevNormToMaxKPH !== normalToMaxKPH) {
+    this.prevNormToMaxKPH = normalToMaxKPH;
+    outlet(1, normalToMaxKPH);        // output to "speedometer"
+  }
 }
